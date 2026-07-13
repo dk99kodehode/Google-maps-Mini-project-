0ed924857cd272acfb4a77be0e522e09f56b711c
dk99kodehode <dk99kodehode@gmail.com>
2026-07-13 09:02:56 +0200

    Add github pages deployment

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-// måtte få hjelp av google (har ikke nokk erfaring med bruk av VITE)
+// måtte få hjelp av google (har ikke nokk erfaring med bruk av VITE )
 
 export const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
