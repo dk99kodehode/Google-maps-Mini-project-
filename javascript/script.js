121900d30d096743347a3e3bf4da162ee4e4223f
dk99kodehode <dk99.kodehode@gmail.com>
2026-07-30 11:29:14 +0200

    minor changes to categories

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,37 @@
 import { apiKey } from "./env.js";
 
+let userLat;
+let userLon;
+
+const map = L.map("map").setView([0, 0], 2);
+L.tileLayer(
+  `https://maps.geoapify.com/v1/tile/carto/{z}/{x}/{y}.png?&apiKey=${apiKey}`,
+  {
+    attribution:
+      'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
+    maxZoom: 20,
+    id: "osm-bright",
+  },
+).addTo(map);
+
+let category = "commercial.supermarket";
+const restaurantCategory = "catering.restaurant";
+
 /*--------ELEMENTER FRA HTML---------*/
 const searchBar = document.getElementById("search");
 const closeBtn = document.getElementById("close-btn");
 const sideBar = document.getElementById("sidebar");
 const sideBarContent = document.getElementById("sidebar-content");
 
+/*-----swap categories button-----*/
+const restaurantBtn = document.getElementById("rest-btn");
+const storeBtn = document.getElementById("store-btn");
+const parkBtn = document.getElementById("park-btn");
+const hotelBtn = document.getElementById("hotel-btn");
+
 /*-----LOADER KUN "SUPERMARKETS" I URL-----*/
-async function loadPlace(lat, lon) {
-  const placeAPI = `https://api.geoapify.com/v2/places?categories=commercial.supermarket&bias=proximity:${lon},${lat}&limit=20&apiKey=${apiKey}`;
+async function loadPlace(lat, lon, category) {
+  const placeAPI = `https://api.geoapify.com/v2/places?categories=${category}&bias=proximity:${lon},${lat}&limit=20&apiKey=${apiKey}`;
 
   const response = await fetch(placeAPI);
 
@@ -29,24 +52,13 @@ navigator.geolocation.getCurrentPosition(
 
     map.setView([userLat, userLon], 14);
 
-    loadPlace(userLat, userLon);
+    loadPlace(userLat, userLon, category);
   },
   // visst den blir "kansellert" default map setview value 0 lat , 0 lon
   (error) => {
     map.setView([0, 0], 2);
   },
 );
-
-const map = L.map("map").setView([0, 0], 2);
-L.tileLayer(
-  `https://maps.geoapify.com/v1/tile/carto/{z}/{x}/{y}.png?&apiKey=${apiKey}`,
-  {
-    attribution:
-      'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
-    maxZoom: 20,
-    id: "osm-bright",
-  },
-).addTo(map);
 
 // Marker som tar lon,lat,names & adress //
 function markerToMap(data) {
@@ -91,4 +103,4 @@ map.addEventListener("click", () => {
 
 ///
 
-loadPlace(userLat, userLon);
+loadPlace(userLat, userLon, category);
